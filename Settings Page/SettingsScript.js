a5cd8fb2b43d4dd629e5975839ffca31d415576d
Dylan McQuaker <mcquakerd@gmail.com>
2025-04-27 03:00:45 +0100

    Added comments.

--- Settings Page/SettingsScript.js
+++ Settings Page/SettingsScript.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('.Menu_Icon');
 const dropdownMenu = document.querySelector('#dropdownMenu');
 
+
+//Dropdown menu customisation so that you can click it to make it appear and dissapear
 button.addEventListener('click', () => {
     dropdownMenu.classList.toggle('active');
 });
@@ -18,12 +20,13 @@ form.addEventListener('change', () => {
     const hints = document.getElementById('hints').value;
     const guesses = document.getElementById('guesses').value;
     const passages = document.querySelector('input[name="passages"]:checked').value;
-
+    //Adding the saved settings to the browser localstorage for ease of use
     localStorage.setItem('Game_Settings', JSON.stringify({ difficulty, rooms, hints, guesses, passages }));
 });
-
+//loading the settings on the page refresh/load and storing it into Saved_Settings
 window.onload = () => {
     const Saved_Settings = JSON.parse(localStorage.getItem('Game_Settings'));
+    // If theres any saved data it will change the value to the saved ones
     if (Saved_Settings) {
         document.querySelector(`input[name="difficulty"][value="${Saved_Settings.difficulty}"]`).checked = true;
         document.getElementById('rooms').value = Saved_Settings.rooms;
